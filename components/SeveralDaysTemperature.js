8bc731966a52e1f6020f9c884c28ca3f64b5f49b
Kirill <mrbomberman99@gmail.com>
2021-09-24 15:59:39 +0300

    added forecast for several days

--- components/SeveralDaysTemperature.js
+++ components/SeveralDaysTemperature.js
@@ -13,15 +13,22 @@ const Container = styled.div`
 function SeveralDaysTemperature({data}) {
 
 
-    let secondDay = data === null ? 'Loading...' : formatDate(data.list[8].dt_txt);
-    let thirdDay = data === null ? 'Loading...' : formatDate(data.list[16].dt_txt);
-    let fourthDay = data === null ? 'Loading...' : formatDate(data.list[24].dt_txt);
-    let fifthDay = data === null ? 'Loading...' : formatDate(data.list[32].dt_txt);
+    let secondDay = data === null ? 'Loading...' : formatDate(data.list[8].dt_txt,data.list[8].weather[0].main, data.list[8].main.humidity);
+    let thirdDay = data === null ? 'Loading...' : formatDate(data.list[16].dt_txt, data.list[16].weather[0].main, data.list[16].main.humidity);
+    let fourthDay = data === null ? 'Loading...' : formatDate(data.list[24].dt_txt, data.list[24].weather[0].main, data.list[24].main.humidity);
+    let fifthDay = data === null ? 'Loading...' : formatDate(data.list[32].dt_txt, data.list[32].weather[0].main, data.list[32].main.humidity);
     
-    function formatDate(date) {
+    function formatDate(date, situation, humidity) {
         let formatedDate = new Date(date).toString().slice(4,10)
 
-        return formatedDate
+        return <div>
+            <p>{formatedDate}</p>
+            <div>{situation}</div>
+            <div>
+                <div>Humidity</div>
+                <div>{humidity + '%'}</div>
+            </div>
+        </div>
     }
 
     return (

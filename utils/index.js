899c56bfe7ddaba06f3f859e28350d8a655615cc
Kirill <mrbomberman99@gmail.com>
2022-01-09 15:20:43 +0300

    made a safe protocol

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,7 @@
 function fetchData() {
 
 
-    return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=London&appid=2061b3a9d510a4c514ba1b661d445337`)
+    return fetch(`https://api.openweathermap.org/data/2.5/forecast?q=London&appid=2061b3a9d510a4c514ba1b661d445337`)
         .then(res => res.json())
 
 

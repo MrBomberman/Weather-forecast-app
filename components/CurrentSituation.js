b63a8181e28f2d930abec4a8200380176fe480c1
Kirill <mrbomberman99@gmail.com>
2022-01-27 17:00:10 +0300

    changed paths of images for state weather

--- components/CurrentSituation.js
+++ components/CurrentSituation.js
@@ -34,28 +34,28 @@ export default function CurrentSituation({data}){
 
     switch(mainCurrentWeather){
         case('Clouds') : 
-        mainCurrentWeather = <Image src='/../public/images/cloud.png'
+        mainCurrentWeather = <Image src='https://cdn-icons.flaticon.com/png/512/3222/premium/3222791.png?token=exp=1643286802~hmac=39739637c9e86d4f4b81fd82f944c168'
         width={100}
         height={100}
         layout="fixed"
         alt='image'/>
         break;
         case('Rain') : 
-        mainCurrentWeather = <Image src='/../public/images/slight-rain.png'
+        mainCurrentWeather = <Image src='https://cdn-icons.flaticon.com/png/512/1585/premium/1585379.png?token=exp=1643292861~hmac=5fb76791a00cf58936695a83438e6677'
         width={100}
         height={100}
         layout="fixed"
         alt='image'/>
         break;
         case('Snow') : 
-        mainCurrentWeather = <Image src='/../public/images/snowfall.png'
+        mainCurrentWeather = <Image src='https://cdn-icons.flaticon.com/png/512/2465/premium/2465979.png?token=exp=1643292806~hmac=54fbc504fee48f812de037997079d392'
         width={100}
         height={100}
         layout="fixed"
         alt='image'/>
         break;
         case('Clear') : 
-        mainCurrentWeather = <Image src='/../public/images/clear-sky.png'
+        mainCurrentWeather = <Image src='https://cdn-icons.flaticon.com/png/512/3222/premium/3222794.png?token=exp=1643292766~hmac=c2bee347a54e38974b899d43caa9ac17'
         width={100}
         height={100}
         layout="fixed"

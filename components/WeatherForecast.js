401f0c5ee96deed7de7e839142ecb5fd038184c8
MrBomberman <mrbomberman99@gmail.com>
2022-04-06 11:09:36 +0300

    changed width and height of container - mobile version

--- components/WeatherForecast.js
+++ components/WeatherForecast.js
@@ -30,8 +30,8 @@ box-shadow: -3px -2px 36px -7px rgba(0,0,0,0.75);
     height: 500px;
     }
 @media only screen and (max-width: 430px){
-    width: 100%;
-    height: 100%;
+    width: 95%;
+    height: 95%;
     border-radius: 0;
     }
 `
